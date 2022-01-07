5f656cf8977bbfc4bff54161c5eb1bfc05ef7a4d
jlxw48 <jlxw48@gmail.com>
2022-01-07 21:59:21 +0800

    add algo

--- src/components/formModal.js
+++ src/components/formModal.js
@@ -5,183 +5,101 @@ import { DialogActions, DialogContent, TextField, Button, IconButton } from '@mu
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
 
-export default function FormModal(props) {
-    const {isOpen, handleClose} = props
-    const [persons, setPersons] = useState([
-        {person: ''}
-    ])
-    const [mainName, setMainName] = useState("")
-    const [eventName, setEventName] = useState("")
-    
+export default function FormModal( props ) {
+    const { isOpen, handleClose } = props
+    const [ persons, setPersons ] = useState( [
+        { person: '' }
+    ] )
+    const [ mainName, setMainName ] = useState( "" )
+    const [ eventName, setEventName ] = useState( "" )
+
     const addField = () => {
-        setPersons([...persons, {person: ''}])
+        setPersons( [ ...persons, { person: '' } ] )
     }
 
-    const removeField = (index) => {
-        let fields = [...persons]
+    const removeField = ( index ) => {
+        let fields = [ ...persons ]
 
-        if (fields.length === 1) {
-            alert("You must split with at least one other person!")
+        if ( fields.length === 1 ) {
+            alert( "You must split with at least one other person!" )
         } else {
-            fields.splice(index, 1)
-            setPersons(fields)
+            fields.splice( index, 1 )
+            setPersons( fields )
         }
     }
 
-    const handleOnChangeField = (index, event) => {
-        const fields = [...persons]
-        fields[index]['person'] = event.target.value;
-        setPersons(fields)
-        console.log(fields)
+    const handleOnChangeField = ( index, event ) => {
+        const fields = [ ...persons ]
+        fields[ index ][ 'person' ] = event.target.value;
+        setPersons( fields )
+        console.log( fields )
     }
 
-    // minimises number of senders
-    const normalSplit = (expensesMap) => {
-        console.log(expensesMap)
-        var total = 0;
-        for (var person in expensesMap) {
-            total += expensesMap[person]
-        }
-        const avgCost = total/Object.keys(expensesMap).length;
-        var personToIndexMap = {}
-        var transaction = []
-        var counter = 0;
-        for (var p in expensesMap) {
-            // how much the person needs to pay/get back from central pot
-            // +ve diff = need to get back, -ve means need to pay
-            var diff = expensesMap[p] - avgCost 
-            console.log("diff", diff)
-            transaction.push([p, diff])
-            personToIndexMap[p] = counter;
-            counter++;
-        }
-        console.log("transaction", transaction)
-        transaction.sort((p1, p2) => p1[1] - p2[1]);
-        console.log("transaction", transaction)
-        var left = 0;
-        var right = transaction.length - 1;
-        while (left < right) {
-            const debtor = transaction[left];
-            const creditor = transaction[right];
-
-            var min = Math.min(-debtor[1], creditor[1]);
-            debtor[1] += min;
-            creditor[1] -= min;
-            console.log(debtor[0], "pays", creditor[0], min)
-
-            if (debtor[1] === 0) {
-                left++;
-            } else if (creditor[1] === 0) {
-                right--;
-            } 
-        }
+    const handleEventChangeField = ( event ) => {
+        setEventName( event.target.value )
     }
 
-    // maximises number of senders, so that each person doesnt need to send too many times
-    const normalSplit2 = (expensesMap) => {
-        var total = 0;
-        for (var person in expensesMap) {
-            total += expensesMap[person]
-        }
-        const avgCost = total/Object.keys(expensesMap).length;
-        var personToIndexMap = {}
-        var transaction = []
-        var counter = 0;
-        for (var p in expensesMap) {
-            // how much the person needs to pay/get back from central pot
-            // +ve diff = need to get back, -ve means need to pay
-            var diff = expensesMap[p] - avgCost 
-            transaction.push([p, diff])
-            personToIndexMap[p] = counter;
-            counter++;
-        }
-        const comparator = (p1, p2) => p1[1] - p2[1];
-        transaction.sort(comparator);
-        var left = 0;
-        var right = transaction.length - 1;
-        while (left < right) {
-            const debtor = transaction[left];
-            const creditor = transaction[right];
-            const transfer = -debtor[1]
-            debtor[1] -= transfer;
-            creditor[1] -= transfer;
-            var newArr = []
-            for (var i = 0; i < transaction.length; i++) {
-                var tuple = transaction[i]
-                if (tuple[1] === 0) {
-                    continue;
-                }
-                newArr.push(tuple)
-            }
-            transaction = newArr
-            transaction.sort(comparator);
-            right = transaction.length - 1
-        }
-    const handleEventChangeField = (event) => {
-        setEventName(event.target.value)
-    }
-
-    const handleMainNameChange = (e) => {
-        setMainName(e.target.value)
+    const handleMainNameChange = ( e ) => {
+        setMainName( e.target.value )
     }
 
     const onFormSubmit = () => {
-        localStorage.setItem('eventName', eventName)
-        localStorage.setItem("persons", JSON.stringify(persons))
-        localStorage.setItem("mainName", mainName)
+        localStorage.setItem( 'eventName', eventName )
+        localStorage.setItem( "persons", JSON.stringify( persons ) )
+        localStorage.setItem( "mainName", mainName )
     }
 
     return (
         <div>
             <Dialog open={isOpen} onClose={handleClose} fullWidth maxWidth='sm'>
-                <DialogTitle sx={{paddingBottom:0}}>New Event</DialogTitle>
+                <DialogTitle sx={{ paddingBottom: 0 }}>New Event</DialogTitle>
                 <DialogContent>
-                    <TextField 
-                        variant='standard' 
-                        autoFocus 
+                    <TextField
+                        variant='standard'
+                        autoFocus
                         margin='dense'
                         fullWidth
                         label="Event Name"
                         value={eventName}
-                        onChange={e => handleEventChangeField(e)}
-                        required={true}/>
+                        onChange={e => handleEventChangeField( e )}
+                        required={true} />
                 </DialogContent>
-                <DialogTitle sx={{paddingBottom:0}}>Who to split?</DialogTitle>
+                <DialogTitle sx={{ paddingBottom: 0 }}>Who to split?</DialogTitle>
                 <DialogContent>
-                        <TextField 
-                            variant='standard' 
-                            autoFocus 
-                            margin='dense'
-                            fullWidth
-                            label="Your Name"
-                            sx={{width:"50%"}}
-                            required={true}
-                            value={mainName}
-                            onChange={e => handleMainNameChange(e)}/>
-                        {persons.map( (personObj, index) => (
-                            <div key={index}>
-                                <TextField 
-                                    variant='standard' 
-                                    autoFocus 
-                                    margin='dense'
-                                    label={"Person " + (index+2)}
-                                    sx={{width:"50%"}}
-                                    value={personObj.person}
-                                    onChange={event => handleOnChangeField(index, event)}
-                                    required={true}/>
-                                <IconButton onClick={() => removeField(index)} size='large' sx={{marginTop: '10px'}}>
-                                    <RemoveIcon />
-                                </IconButton>
-                                <IconButton onClick={addField} size='large' sx={{marginTop: '10px'}}>
-                                    <AddIcon />
-                                </IconButton>   
-                            </div> 
-                        ))}
+                    <TextField
+                        variant='standard'
+                        autoFocus
+                        margin='dense'
+                        fullWidth
+                        label="Your Name"
+                        sx={{ width: "50%" }}
+                        required={true}
+                        value={mainName}
+                        onChange={e => handleMainNameChange( e )} />
+                    {persons.map( ( personObj, index ) => (
+                        <div key={index}>
+                            <TextField
+                                variant='standard'
+                                autoFocus
+                                margin='dense'
+                                label={"Person " + ( index + 2 )}
+                                sx={{ width: "50%" }}
+                                value={personObj.person}
+                                onChange={event => handleOnChangeField( index, event )}
+                                required={true} />
+                            <IconButton onClick={() => removeField( index )} size='large' sx={{ marginTop: '10px' }}>
+                                <RemoveIcon />
+                            </IconButton>
+                            <IconButton onClick={addField} size='large' sx={{ marginTop: '10px' }}>
+                                <AddIcon />
+                            </IconButton>
+                        </div>
+                    ) )}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={onFormSubmit} href='/summary'>Next</Button>
                 </DialogActions>
-            </Dialog>            
+            </Dialog>
         </div>
     )
 }
--- src/pages/summary.js
+++ src/pages/summary.js
@@ -2,9 +2,102 @@ import React from 'react'
 
 export default function Summary() {
 
-    const mainName = localStorage.getItem("mainName")
-    const persons = JSON.parse(localStorage.getItem("persons"))
-    const eventName = localStorage.getItem("eventName")
+    const mainName = localStorage.getItem( "mainName" )
+    const persons = JSON.parse( localStorage.getItem( "persons" ) )
+    const eventName = localStorage.getItem( "eventName" )
+
+    const processJson = (spendings) => {
+        const expensesMap = {}
+        for (var i = 0; i < spendings.length; i++) {
+            expensesMap[spendings.person] = spendings.amount;
+        }
+        return expensesMap
+    }
+
+    // minimises number of senders
+    const normalSplit = ( expensesMap ) => {
+        console.log( expensesMap )
+        var total = 0;
+        for ( var person in expensesMap ) {
+            total += expensesMap[ person ]
+        }
+        const avgCost = total / Object.keys( expensesMap ).length;
+        var personToIndexMap = {}
+        var transaction = []
+        var counter = 0;
+        for ( var p in expensesMap ) {
+            // how much the person needs to pay/get back from central pot
+            // +ve diff = need to get back, -ve means need to pay
+            var diff = expensesMap[ p ] - avgCost
+            console.log( "diff", diff )
+            transaction.push( [ p, diff ] )
+            personToIndexMap[ p ] = counter;
+            counter++;
+        }
+        console.log( "transaction", transaction )
+        transaction.sort( ( p1, p2 ) => p1[ 1 ] - p2[ 1 ] );
+        console.log( "transaction", transaction )
+        var left = 0;
+        var right = transaction.length - 1;
+        while ( left < right ) {
+            const debtor = transaction[ left ];
+            const creditor = transaction[ right ];
+
+            var min = Math.min( -debtor[ 1 ], creditor[ 1 ] );
+            debtor[ 1 ] += min;
+            creditor[ 1 ] -= min;
+            console.log( debtor[ 0 ], "pays", creditor[ 0 ], min )
+
+            if ( debtor[ 1 ] === 0 ) {
+                left++;
+            } else if ( creditor[ 1 ] === 0 ) {
+                right--;
+            }
+        }
+    }
+
+    // maximises number of senders, so that each person doesnt need to send too many times
+    const minimiseInconvenience = ( expensesMap ) => {
+        var total = 0;
+        for ( var person in expensesMap ) {
+            total += expensesMap[ person ]
+        }
+        const avgCost = total / Object.keys( expensesMap ).length;
+        var personToIndexMap = {}
+        var transaction = []
+        var counter = 0;
+        for ( var p in expensesMap ) {
+            // how much the person needs to pay/get back from central pot
+            // +ve diff = need to get back, -ve means need to pay
+            var diff = expensesMap[ p ] - avgCost
+            transaction.push( [ p, diff ] )
+            personToIndexMap[ p ] = counter;
+            counter++;
+        }
+        const comparator = ( p1, p2 ) => p1[ 1 ] - p2[ 1 ];
+        transaction.sort( comparator );
+        var left = 0;
+        var right = transaction.length - 1;
+        while ( left < right ) {
+            const debtor = transaction[ left ];
+            const creditor = transaction[ right ];
+            const transfer = -debtor[ 1 ]
+            debtor[ 1 ] -= transfer;
+            creditor[ 1 ] -= transfer;
+            var newArr = []
+            for ( var i = 0; i < transaction.length; i++ ) {
+                var tuple = transaction[ i ]
+                if ( tuple[ 1 ] === 0 ) {
+                    continue;
+                }
+                newArr.push( tuple )
+            }
+            transaction = newArr
+            transaction.sort( comparator );
+            right = transaction.length - 1
+        }
+    }
+
     return (
         <div>
             <h1>This is the summary page</h1>
